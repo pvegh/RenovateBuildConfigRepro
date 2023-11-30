bf03a9d820cc19e0d6a8b1fd789f179eee1b6782
Peter Vegh <peter.vegh@dm.de>
2023-11-30 12:47:10 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.renovatebuildconfigrepro"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.renovatebuildconfigrepro"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        buildConfigField("int", "FOO_VERSION_CODE", "1")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
